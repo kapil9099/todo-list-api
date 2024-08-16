07a75c07b8a6644e3b020eaf61b7dd35880d98bd
Kapil Jethwani <63433624+kapil9099@users.noreply.github.com>
2024-08-16 13:48:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
-// Middleware
+
 app.use(bodyParser.json());
 
-// Routes
+
 app.use('/api', taskRoutes);
 
 module.exports = app;
